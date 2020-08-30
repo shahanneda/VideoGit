3180775474da8d1d67747ec91035cd33729e7360
Shahan Neda <shahan.neda@gmail.com>
2020-08-30 13:35:05 -0700

    fixed bug with big wpm

--- ncodevideo.py
+++ ncodevideo.py
@@ -1,8 +1,8 @@
 
 import subprocess
 import re
-import ffmpeg
 import sys;
+import math;
 
 
 get_log_command = "git log --pretty=format:\"%h %s\"";
@@ -13,7 +13,7 @@ batcmd="git status"
 commit1="7d1633a"
 commit2="f2217af"
 
-wpm = 240;
+wpm = 480;
 chars_per_second = wpm * 5 / 60; #5 char/word * 1min/60sec
 
 
@@ -34,16 +34,15 @@ class ncodevideo:
     def loop_through_file_paths(self, file_paths, starting_commit, ending_commit):
         for file_path in file_paths:
             try:
-                inital_file = self.run_system_command(f"git show {starting_commit}:{file_path}");
                 diff_of_file = self.run_system_command(f"git diff {starting_commit}..{ending_commit} {file_path}")
             except:
                 print("Failed to open file: " + file_path);
                 continue;
             # print(diff_of_file);
-            self.handle_file_diffs(inital_file, diff_of_file)
+            self.handle_file_diffs(diff_of_file)
 
 
-    def handle_file_diffs(self, inital_file, diff_of_file):
+    def handle_file_diffs(self, diff_of_file):
         lines_of_diffs = str.split(diff_of_file, "\n");
         file_name = lines_of_diffs[2][5:] # get second line (has filename) and remove the - space spcae
         file_name = file_name.split("/")[len(file_name.split("/")) - 1] # remove the directory like /gg/gg/g and just get the last part
@@ -86,11 +85,13 @@ class ncodevideo:
         completed_code_buffer = [];
         longest_line = 0;
 
-        frames_per_char = int(frame_rate / chars_per_second);
+        frames_per_char = frame_rate / chars_per_second;
 
+        real_frame_rate = frame_rate/ frames_per_char;
+        print(f" framerate: {frame_rate}, frames_per_char: {frames_per_char} chars_per_second: {chars_per_second} real_frame_rate: {real_frame_rate}");
         for line_number, line in lines_to_be_added.items():
             number_of_frames_needed = int((len(line) / chars_per_second) * frame_rate); # how many frames needed to add/remove line
-            for i in range(0,number_of_frames_needed, frames_per_char):
+            for i in range(0,number_of_frames_needed, math.ceil(frames_per_char)):
                 if line_number in indices_of_lines_to_be_removed: # check if its marked to be removed
                     # handle_image_creation_remove_line();
                     if len(line) >0:
@@ -165,7 +166,7 @@ class ncodevideo:
         #     pass;
         #TODO: use stdin to optimise
         extension =  file_name.split(".")[-1]; # get the file extension
-        with open("{temp_location}/temp_code.txt", "w") as text_file:
+        with open(f"{temp_location}/temp_code.txt", "w") as text_file:
             print(code, file=text_file, end="")
         self.run_system_command(f"{silicon_command}/{file_name}{index_of_image}.png --language {extension}"); # make master copy
 
