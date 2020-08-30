981762e95979c7e0cee41839069ddbd70355c0aa
Shahan Neda <shahan.neda@gmail.com>
2020-08-29 17:52:23 -0700

    setup basic commands and class

--- ncodevideo.py
+++ ncodevideo.py
@@ -1,8 +1,41 @@
 
-
-
 import subprocess
 
+
+getLogCommand = "git log --pretty=format:\"%h %s\"";
 batcmd="git status"
-result = subprocess.check_output(batcmd, shell=True)
-print(result)
+commit1="700043f"
+commit2="3f8d661"
+class ncodevideo:
+    def runSystemCommand(self, command):
+        return subprocess.check_output(command, shell=True, text=True)
+
+    def __init__(self):
+        findChangedFilePaths = f"git diff --name-only {commit1}..{commit2}";
+        filePaths = str.split(self.runSystemCommand(findChangedFilePaths), "\n");
+        self.loopThroughFiles(filePaths, commit1, commit2);
+
+    def loopThroughFiles(self, filePaths, startingCommit, endingCommit):
+        for filePath in filePaths:
+            print(filePath)
+            try:
+                initalFile =  self.runSystemCommand(f"git show {startingCommit}:{filePath}");
+            except:
+                print("Failed to open file: " + filePath);
+                continue;
+
+            print(initalFile)
+
+
+
+
+
+
+
+
+
+
+nv = ncodevideo();
+
+
+
