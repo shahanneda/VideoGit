e551795183952368ea76bf167253f2929d4d65e6
Shahan Neda <shahan.neda@gmail.com>
2020-08-29 20:40:30 -0700

    added basic ffmpeg

--- ncodevideo.py
+++ ncodevideo.py
@@ -1,6 +1,7 @@
 
 import subprocess
 import re
+import ffmpeg
 
 
 get_log_command = "git log --pretty=format:\"%h %s\"";
@@ -8,6 +9,8 @@ silicon_command = "silicon temp_code.txt --language cpp --output temp-img/";
 batcmd="git status"
 commit1="700043f"
 commit2="3f8d661"
+
+frame_rate = 30;
 class ncodevideo:
     def run_system_command(self, command):
         return subprocess.check_output(command, shell=True, text=True)
@@ -42,9 +45,11 @@ class ncodevideo:
         lines_without_diff = lines_of_diffs;
         lines_to_be_removed = {}; # we need to store this so we can add them later
 
+
         for i, line in enumerate(lines_of_diffs):
             if(len(line) > 0 and line[0] == "+"): # remove the lines that have addidiotsn, were going to add them later
-                lines_to_be_removed.put(i, line);
+                line = line[1:]
+                lines_to_be_removed[i] =  line;
 
             if(len(line) > 0 and line[0] == "-"): # remove the -  
                 lines_without_diff[i] = line[1:];
@@ -52,23 +57,30 @@ class ncodevideo:
 
         # remove all the lines queed up in lines_to_be_removed (the + ones)
         lines_without_diff = [line for i, line in enumerate(lines_without_diff) if i not in lines_to_be_removed] 
-
-        full_code = "\n".join(lines_without_diff);
-        full_code = re.sub(r'@@(.*?)@@', "", full_code); # this regex is to remove the git hulls which are @@ int ... @@
-
-        print(lines_to_be_removed);
-        self.handle_file_incrementing(full_code, lines_to_be_removed, file_name);
+        self.handle_file_incrementing(lines_without_diff, lines_to_be_removed, file_name);
 
 
 
-    def handle_file_incrementing(self, full_code, lines_to_be_removed, file_name):
+    def handle_file_incrementing(self, file_in_list_form, lines_to_be_added, file_name):
+        index_of_images = 0; # for creating the video frames
+        print(lines_to_be_added);
+        for line_number, line in lines_to_be_added.items():
+            print(line)
+            file_in_list_form = file_in_list_form[:line_number] + [line] + file_in_list_form[line_number:] # add line
+
+            full_code = "\n".join(file_in_list_form);
+            full_code = re.sub(r'@@(.*?)@@', "", full_code); # this regex is to remove the git hulls which are @@ int ... @@
+            index_of_images+=1;
+
+            self.make_image_from_code(full_code, file_name, index_of_images);
 
 
-        self.make_image_from_code(full_code, file_name, 0);
+    def convert_images_to_video(self, file_name):
+        ffmpeg.input(f'/path/to/jpegs/{file_name}*.png', pattern_type='glob', framerate=frame_rate).output(file_name + '.mp4').run());
 
 
-    def make_image_from_code(self, code, file_name, index_of_image): # index is for the video file
-        # try:
+        def make_image_from_code(self, code, file_name, index_of_image): # index is for the video file
+            # try:
         #     self.run_system_command("mkdir temp-img") # todo move this to its proper place
         # except:
         #     pass;
